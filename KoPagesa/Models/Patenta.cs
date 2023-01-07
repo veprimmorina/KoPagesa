f58b5a8c2aa20eacbfe6ce851c3428928e1a551f
veprimmorina <65460301+veprimmorina@users.noreply.github.com>
2023-01-07 22:12:15 +0100

    patenta

---
+++ KoPagesa/Models/Patenta.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KoPagesa.Models
+{
+    public class Patenta
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Column]
+        public string Fotografia { get; set; }
+
+        [Column (TypeName ="nvarchar (25)")]
+        public string Emri { get; set; }
+
+        [Column (TypeName ="nvarchar (25)")]
+        public string Mbiemri { get; set; }
+
+        [Column]
+        public string DataLindjes { get; set; }
+
+        [Column (TypeName = "nvarchar (45)")]
+        public string NumriPersonal { get; set; }
+
+        [Column (TypeName = "nvarchar (25)")]
+        public string Komuna { get; set; }
+
+        [Column (TypeName = "nvarchar (25)")]
+        public string DataLeshimit { get; set; }
+
+        [Column (TypeName ="nvarchar (25)")]
+        public string DataSkadences { get; set; }
+
+        [Column]
+        public Boolean EAktivizuar { get; set;}
+
+
+    }
+}
---
+++ KoPagesa/Models/Perdoruesi.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KoPagesa.Models
+{
+    public class Perdoruesi
+    {
+        [Key]
+        public int PerdoruesiId { get; set; }
+
+        [Column (TypeName = "nvarchar (25)")]
+        public string Emri { get; set; }
+
+        [Column(TypeName = "nvarchar (25)")]
+        public string Mbiemri { get; set; }
+
+        [Column(TypeName = "nvarchar (25)")]
+        public string Emaili { get; set; }
+
+        [Column(TypeName = "nvarchar (30)")]
+        public string Fjalkalimi { get; set; }
+        [Column]
+        public int Roli { get; set; }
+
+
+    }
+}
